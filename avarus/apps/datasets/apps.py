a87b18bbbeda503c1a73f3dc773666d3ba515a27
Ефремов Виктор Васильевич <vvefremov_1@edu.hse.ru>
2024-04-24 18:19:19 +0300

    feat: auto create map_location when adding dataset
    TODO: authors are not set bc it's m2m field and post_save don't track
    them

--- avarus/apps/datasets/apps.py
+++ avarus/apps/datasets/apps.py
@@ -3,3 +3,6 @@ from django.apps import AppConfig
 
 class DatasetsConfig(AppConfig):
     name = 'apps.datasets'
+
+    def ready(self):
+        import apps.datasets.signals
---
+++ avarus/apps/datasets/signals.py
@@ -0,0 +1,12 @@
+from apps.datasets.models import Dataset
+from apps.map_locations.models import MapLocation
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+
+@receiver(post_save, sender=Dataset)
+def create_map_location(sender, instance, created, **kwargs):
+    ''' Create corresponding map location on dataset creation '''
+    if created:
+        loc = MapLocation.objects.create(name=instance.title, dataset=instance)
+        # TOOO authors are not set bc it's m2m field and post_save don't track them
