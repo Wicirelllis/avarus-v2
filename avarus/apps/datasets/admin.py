c7967ecf340e3b362c0a30e5db4a303f8de45378
Ефремов Виктор Васильевич <vvefremov_1@edu.hse.ru>
2024-04-25 13:00:02 +0300

    style: clean dataset model
    Remove unused fields.
    Reorganize admin page, hide autofilled fields under spoiler.

--- avarus/apps/datasets/admin.py
+++ avarus/apps/datasets/admin.py
@@ -4,6 +4,51 @@ from .models import Dataset
 
 
 class DatasetAdmin(admin.ModelAdmin):
-    pass
+    fieldsets = [
+        (
+            None,
+            {
+                'description': 'Main fields. While they are optional it is recommended to fill them.',
+                'fields': [
+                    'title',
+                    'summary',
+                    'env',
+                    'spp',
+                    'image',
+                    'authors',
+                    'publications',
+                    'status',
+                    'permafrost_type',
+                    'permafrost_data',
+                    'additional_data',
+                ],
+            },
+        ),
+        (
+            'Optional fields',
+            {
+                'description': 'Fields below autofill based on dataset files content. But feel free to fill / edit if necessities arise.',
+                'classes': [
+                    'collapse',
+                ],
+                'fields': [
+                    'year',
+                    'n_plots',
+                    'coverscale',
+                    'longitude',
+                    'latitude',
+                    'geotagged',
+                    'region',
+                    'location',
+                    'subzone',
+                    'mosses',
+                    'liverworts',
+                    'liches',
+                    'vascular',
+                    'cryptogam'
+                ],
+            },
+        ),
+    ]
 
 admin.site.register(Dataset, DatasetAdmin)
--- avarus/apps/datasets/models.py
+++ avarus/apps/datasets/models.py
@@ -4,55 +4,51 @@ from apps.publications.models import Publication
 from django.db import models
 from django.urls import reverse
 
+LOAN_STATUS = (
+    ('pu', 'Public'),
+    ('pr', 'Private')
+)
+
 
 class Dataset(models.Model):
-    LOAN_STATUS = (
-        ('pu', 'Public'),
-        ('pr', 'Private')
-    )
-    title = models.CharField(max_length=200, blank=True, null=True)
-    summary = models.TextField(max_length=1000, help_text="Enter a brief description of the book", blank=True)
+    title = models.CharField(max_length=200, blank=True)
+    summary = models.TextField(help_text="A brief description of the dataset.", blank=True)
 
     env = models.FileField(upload_to='datasets/env/', blank=True)
     spp = models.FileField(upload_to='datasets/spp/', blank=True)
-
-    dataset = models.CharField(max_length=200, blank=True)
-    dataset_env = models.CharField(blank=True, max_length=200)
-    status = models.CharField(max_length=5, choices=LOAN_STATUS, blank=True, default='pr', help_text='Book availability')
     image = models.ImageField(upload_to='datasets/img/', blank=True)
 
-    authors = models.ManyToManyField(Author, related_name='datasets', null=True, blank=True)
-    publications = models.ManyToManyField(Publication, related_name='datasets', null=True, blank=True)
+    authors = models.ManyToManyField(Author, related_name='datasets', blank=True)
+    publications = models.ManyToManyField(Publication, related_name='datasets', blank=True)
 
-    name = models.CharField(max_length=200, blank=True, null=True, help_text='Name of dataset', verbose_name='Verbose')
-    year = models.CharField(max_length=200, blank=True, null=True, help_text='Year of creation')
-    n_plots = models.CharField(max_length=200, blank=True, null=True, help_text='Number of plots')
-    coverscale = models.CharField(max_length=200, blank=True, null=True, help_text='')
+    status = models.CharField(max_length=5, choices=LOAN_STATUS, blank=True, default='pr', help_text='Availability.')
+    permafrost_type = models.CharField(max_length=200, blank=True, help_text='')
+    permafrost_data = models.CharField(max_length=200, blank=True, help_text='')
+    additional_data = models.TextField(blank=True, help_text='Any additional info related to dataset.')
 
+
+    year = models.CharField(max_length=200, blank=True, help_text='Year of creation')
+    n_plots = models.CharField(max_length=200, blank=True, help_text='Number of plots')
+    coverscale = models.CharField(max_length=200, blank=True, help_text='')
     longitude = models.FloatField(blank=True, null=True)
     latitude = models.FloatField(blank=True, null=True)
-
-    coordinates = models.CharField(max_length=200, blank=True, null=True, help_text='Key site coordinates')
-    geotagged = models.CharField(max_length=200, blank=True, null=True, help_text='')
-    region = models.CharField(max_length=200, blank=True, null=True, help_text='')
-    location = models.CharField(max_length=200, blank=True, null=True, help_text='')
-    subzone = models.CharField(max_length=200, blank=True, null=True, help_text='')
-    permafrost_type = models.CharField(max_length=200, blank=True, null=True, help_text='')
-    permafrost_data = models.CharField(max_length=200, blank=True, null=True, help_text='')
-    additional_data = models.CharField(max_length=200, blank=True, null=True, help_text='')
-    mosses = models.CharField(max_length=200, blank=True, null=True, help_text='')
-    liverworts = models.CharField(max_length=200, blank=True, null=True, help_text='')
-    liches = models.CharField(max_length=200, blank=True, null=True, help_text='')
-    vascular = models.CharField(max_length=200, blank=True, null=True, help_text='')
-    cryptogam = models.CharField(max_length=200, blank=True, null=True, help_text='')
+    geotagged = models.CharField(max_length=200, blank=True, help_text='')
+    region = models.CharField(max_length=200, blank=True, help_text='')
+    location = models.CharField(max_length=200, blank=True, help_text='')
+    subzone = models.CharField(max_length=200, blank=True, help_text='')
+    mosses = models.CharField(max_length=200, blank=True, help_text='')
+    liverworts = models.CharField(max_length=200, blank=True, help_text='')
+    liches = models.CharField(max_length=200, blank=True, help_text='')
+    vascular = models.CharField(max_length=200, blank=True, help_text='')
+    cryptogam = models.CharField(max_length=200, blank=True, help_text='')
 
     # data for charts
-    disturban = models.JSONField(blank=True, null=True, default=dict)
-    position = models.JSONField(blank=True, null=True, default=dict)
-    soil_text = models.JSONField(blank=True, null=True, default=dict)
-    ecotope = models.JSONField(blank=True, null=True, default=dict)
-    phytocoenosis = models.JSONField(blank=True, null=True, default=dict)
-    phytocoenosis_cover = models.JSONField(blank=True, null=True, default=dict)
+    disturban = models.JSONField(blank=True, default=dict)
+    position = models.JSONField(blank=True, default=dict)
+    soil_text = models.JSONField(blank=True, default=dict)
+    ecotope = models.JSONField(blank=True, default=dict)
+    phytocoenosis = models.JSONField(blank=True, default=dict)
+    phytocoenosis_cover = models.JSONField(blank=True, default=dict)
 
     def __str__(self):
         return self.title
@@ -61,7 +57,7 @@ class Dataset(models.Model):
         return reverse('dataset-detail', args=[str(self.id)])
     
     def save(self, *args, **kwargs):
-        ''' Overrie save method to automatically generate ddescription of dataset '''
+        ''' Overrie save method to automatically generate description of dataset '''
         super(Dataset, self).save(*args, **kwargs)
         fields = [
             'n_plots',
