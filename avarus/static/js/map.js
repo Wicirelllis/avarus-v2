ded130c9429431a1c2c887c34de077ea7e0e16a3
Ефремов Виктор Васильевич <vvefremov_1@edu.hse.ru>
2024-04-29 22:44:04 +0300

    feat: add static css/js

---
+++ avarus/static/js/map.js
@@ -0,0 +1,78 @@
+ymaps.ready(init);
+
+function init() {
+
+    // Создание экземпляра карты.
+    var map = new ymaps.Map('map',
+        {
+            center: [70.385555, 68.410034],
+            zoom: 5,
+            controls: ['zoomControl'],
+            behaviors: ['drag', 'scrollZoom'],
+            type: 'yandex#satellite',
+        },
+        {
+            searchControlProvider: 'yandex#search',
+        }),
+
+        // Контейнер для меню.
+
+        menu = $('<ul class="menu"></ul>');
+    for (var i = 0, l = groups.length; i < l; i++) {
+        createMenuGroup(groups[i]);
+    }
+
+    function createMenuGroup(group) {
+        // Пункт меню.
+        var menuItem = $('<li class="menuItem"><a type="button">' + group.name + '</a></li>'),
+            collection = new ymaps.GeoObjectCollection(null, {preset: group.style}),
+            submenu = $('<ul class="submenu list-group"></ul>');
+
+        // Добавляем коллекцию на карту.
+        map.geoObjects.add(collection);
+        // Добавляем подменю.
+        menuItem.append(submenu).appendTo(menu)
+
+        for (var j = 0, m = group.items.length; j < m; j++) {
+            createSubMenu(group.items[j], collection, submenu);
+        }
+    }
+
+    function createSubMenu(item, collection, submenu) {
+        // Пункт подменю.
+        var submenuItem = $('<li class="subItem list-group-item"><a type="button">' + item.name + '</a></li>'),
+            // Создаем метку.
+            placemark = new ymaps.Placemark(item.center, {
+                    balloonContentHeader: item.ContentHeader.join(''),
+                    // Зададим содержимое основной части балуна.
+                    balloonContentBody: item.ContentBody.join(''),
+                    // Зададим содержимое нижней части балуна.
+                    balloonContentFooter: item.ContentFooter.join(''),
+                    // Зададим содержимое всплывающей подсказки.
+                    hintContent: item.hint.join(''),
+                },{
+                balloonPane: 'outerBalloon',
+                balloonShadowPane: 'shadows'
+                }
+            );
+
+        // Добавляем метку в коллекцию.S
+        collection.add(placemark)
+        // Добавляем пункт в подменю.
+        submenuItem
+            .appendTo(submenu)
+            // При клике по пункту подменю открываем/закрываем баллун у метки.
+            .bind('click', function () {
+                map.panTo([item.center[0], item.center[1]], {flying: true, duration: 800})
+                return false;
+            })
+            .bind('click', function () {
+                if (!placemark.balloon.isOpen()) {
+                    placemark.balloon.open();
+                }
+            })
+    }
+
+    $(menu).appendTo("#menu_list");
+
+}
---
+++ avarus/static/js/statistics.js
@@ -0,0 +1,48 @@
+$('.mobile-menu').on('click', function (e){
+    e.preventDefault();
+    $('.menu-btn').toggleClass('menu-active')
+    $('.nav-panel').toggleClass('menu-active')
+})
+
+
+$('.map-box').on('click', function (e){
+    e.preventDefault();
+    $('.map-box-btn').toggleClass('map-active')
+    $('.resize').toggleClass('map-active')
+})
+$('.subItem .button').on('click', function (e){
+    e.preventDefault();
+    $('.resize').toggleClass('map-move')
+})
+document.querySelector('#elastic_statistics').oninput = function () {
+    let val = this.value.trim();
+    let elasticItems = document.querySelectorAll('.list-group-item__statistics-column');
+    if (val) {
+        elasticItems.forEach(function (elem) {
+            if (elem.innerText.search(val) == -1) {
+                elem.style.display = "none";
+                // elem.innerHTML = elem.innerText;
+            } else {
+                elem.style.display = "block";
+                let str = elem.innerText;
+            }
+        });
+    } else {
+        elasticItems.forEach(function (elem) {
+                elem.style.display = "block";
+            }
+        );
+    }
+}
+
+document. getElementById('select-all'). onclick = function() {
+    var checkboxes = document. querySelectorAll('input[name="columns"]');
+    for (var checkbox of checkboxes) {
+        checkbox. checked = this. checked;
+    }}
+
+document. getElementById('select-all-env'). onclick = function() {
+    var checkboxes = document. querySelectorAll('input[name="columns_env"]');
+    for (var checkbox of checkboxes) {
+        checkbox. checked = this. checked;
+    }}
