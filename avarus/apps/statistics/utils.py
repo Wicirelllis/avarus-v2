76ae5803b18310e5de1abe3180b486b9e135fd93
Ефремов Виктор Васильевич <vvefremov_1@edu.hse.ru>
2024-06-14 22:19:41 +0300

    fix: natural sort
    When creating sorted list of choices for a form use natural sort, e.g.
    ['1', '11', '2'] is alphabetical sort, ['1', '2', '11'] is natural. To
    do so pad every number in the string with zeros so alphabetical and
    natural sortings will be the same.

--- avarus/apps/statistics/utils.py
+++ avarus/apps/statistics/utils.py
@@ -7,22 +7,9 @@ from apps.datasets.utils import read_env, read_spp
 from django.db.models.query import QuerySet
 
 
-def _atof(s: str) -> float | str:
-    try:
-        return float(s)
-    except ValueError:
-        return s
-
-
-def _alphanum_key(s: str):
-    '''
-    Turn a string into a list of string and number chunks.
-
-    >>> alphanum_key('Ks_106(1)')
-    ['Ks_', 106.0, '(', 1.0, ')']
-
-    '''
-    return [_atof(c) for c in re.split(r'[+-]?([0-9]+(?:[.][0-9]*)?|[.][0-9]+)', s)]
+def _normalize(s: str) -> str:
+    ''' Pad numbers in string with zeros so alphabetical sort will work as natural sort '''
+    return ''.join([i.zfill(8) for i in re.split(r'([0-9]+)', s)])
 
 
 def get_available_datasets(user):
@@ -38,7 +25,7 @@ def make_choices(iter: QuerySet | Iterable[str], sort: bool = True) -> tuple[str
         result = tuple((str(i.pk), str(i)) for i in iter)
     else:
         result = tuple((str(i), str(i)) for i in iter)
-    return sorted(result, key=lambda x: _alphanum_key(x[1])) if sort else result
+    return sorted(result, key=lambda x: _normalize(x[1])) if sort else result
 
 
 def get_spp_df_by_id(id: int | str) -> pd.DataFrame:
